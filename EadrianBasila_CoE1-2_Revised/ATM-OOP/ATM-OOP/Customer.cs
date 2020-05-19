dd2045dbcc70bf1c505b9e1dce1ea001158d1943
Eadrian Basila <basilaeadrian@gmail.com>
2020-05-20 01:12:41 +0800

    Add files via upload

---
+++ EadrianBasila_CoE1-2_Revised/ATM-OOP/ATM-OOP/Customer.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_OOP
+{
+    class Customer
+    {
+        public string Name { get; set; }
+        public string First_Name { get; set; }
+        public string Middle_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Passcode { get; set; }
+        public int Balance { get; set; }
+    }
+}
